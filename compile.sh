a8dc3fbdd1b65b739215b61feef4d914e53be694
Marcin Łagowski <marcinlagowski92@gmail.com>
2020-04-05 03:01:38 +0200

    fixed build.yaml

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-xelatex marcin_lagowski_cv.tex -interaction=nonstopmode
+xelatex -interaction=nonstopmode marcin_lagowski_cv.tex
 
 #rm *.aux *.bbl *.blg *.lof *.log *.lot *.toc *.out *.fls 
 
